ca64ef4448e1197dd3bae11de9fee0f77ab864eb
Juan Diego <diego-415-@hotmail.com>
2020-03-02 16:25:35 -0500

    arreglar el mismo asqueroso error de siempre

--- includes/database.php
+++ includes/database.php
@@ -2,7 +2,7 @@
 
     try
     {
-        $conn=new PDO("mysql:host=sql157.main-hosting.eu; dbname=u658245195_banco", "u658245195_banco", "TInLYPff");
+        $conn=new PDO("mysql:127.0.0.1; dbname=u658245195_banco", "u658245195_banco", "TInLYPff");
         $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     }
     catch (Exception $e)
